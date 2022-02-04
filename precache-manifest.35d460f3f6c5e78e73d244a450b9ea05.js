124b3f17df7178fa575e50e317f9df6b1a188941
sulphitic786 <wqasimg@gmail.com>
2022-02-04 12:45:31 +0500

    Updates

---
+++ precache-manifest.35d460f3f6c5e78e73d244a450b9ea05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407471efd0b987feca0373e53a400469",
+    "url": "/Systematic/index.html"
+  },
+  {
+    "revision": "76974213d77e59f74d4a",
+    "url": "/Systematic/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "ed97f8bc2f3cd2e9dd8c",
+    "url": "/Systematic/static/css/main.9b9f0e3d.chunk.css"
+  },
+  {
+    "revision": "76974213d77e59f74d4a",
+    "url": "/Systematic/static/js/2.90ce9971.chunk.js"
+  },
+  {
+    "revision": "ed97f8bc2f3cd2e9dd8c",
+    "url": "/Systematic/static/js/main.e7ff010f.chunk.js"
+  },
+  {
+    "revision": "1a30bf92f83e32d6a2de",
+    "url": "/Systematic/static/js/runtime~main.c2100c1a.js"
+  },
+  {
+    "revision": "2242dc06ef35aa0dd2a07f2c39bd8470",
+    "url": "/Systematic/static/media/Brownie.2242dc06.jpg"
+  }
+]);
