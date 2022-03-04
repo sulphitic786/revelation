92d79d1ea23c846cf67fb39656316a3a1c8dfb8d
Waseem Qasim <70519281+sulphitic786@users.noreply.github.com>
2022-03-04 08:38:55 +0500

    Add files via upload
    Build folder uploaded

---
+++ precache-manifest.ec7adf4d24ec643a0662c1b8bc5003bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81bdbfb5af8352ad386e230feeddee10",
+    "url": "/Systematic/index.html"
+  },
+  {
+    "revision": "8f7ef8b0fe880052cb65",
+    "url": "/Systematic/static/css/2.94b5edfc.chunk.css"
+  },
+  {
+    "revision": "8e8660b025bee4381b4c",
+    "url": "/Systematic/static/css/main.0c1c2565.chunk.css"
+  },
+  {
+    "revision": "8f7ef8b0fe880052cb65",
+    "url": "/Systematic/static/js/2.d0a21422.chunk.js"
+  },
+  {
+    "revision": "8e8660b025bee4381b4c",
+    "url": "/Systematic/static/js/main.585706d7.chunk.js"
+  },
+  {
+    "revision": "1a30bf92f83e32d6a2de",
+    "url": "/Systematic/static/js/runtime~main.c2100c1a.js"
+  }
+]);
